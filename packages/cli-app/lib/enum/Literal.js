b99ebecf1ac88176c2f6e05b6ffe3870920b1c7d
Rockie Guo <rockie@kitmi.com.au>
2018-10-15 01:00:54 +1100

    Remove unused files.

--- packages/cli-app/lib/enum/Literal.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  DEFAULT_CONFIG_PATH: 'conf',
-  MIDDLEWARES_PATH: 'middlewares',
-  FEATURES_PATH: 'features',
-  APP_CFG_NAME: 'app',
-  DEFAULT_BOOTSTRAP_PATH: 'bootstrap'
-};
